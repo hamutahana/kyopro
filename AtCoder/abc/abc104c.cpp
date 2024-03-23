80e72be7851eb6591f47260229f1dc5e55df9def
hamutahana <164027820+hamutahana@users.noreply.github.com>
2024-03-24 02:11:25 +0900

    コードリファクタとコメント修正 (#2)

--- AtCoder/abc/abc104c.cpp
+++ AtCoder/abc/abc104c.cpp
@@ -8,7 +8,7 @@ int main() {
   cin >> D >> G;
   vi N(D+1), P(D+1);
   rep(i, D) cin >> N.at(i) >> P.at(i);
-  
+
   int ans = 1001;
   for (int i = 0; i < (1 << D); i++) {
     int score = 0, count = 0, rest_num;
@@ -17,11 +17,12 @@ int main() {
         score +=  (100 * j) * N.at(j) + P.at(j);
         count += N.at(j);
       } else {
+        // コンプリートした問題の合計が目標スコアに達していない場合、未コンプリートの問題が一部解けた場合を考える
         // ループ終了後、コンプリートしていない問題番号の最大値が保持されている
         rest_num = j;
       }
     }
-    
+
     if (score >= G) {
       ans = min(ans, count);
     } else {
@@ -35,3 +36,4 @@ int main() {
   }
   cout << ans << endl;
 }
+
