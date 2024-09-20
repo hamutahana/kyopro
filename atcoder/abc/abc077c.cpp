3711f182717a549aff1389a5c3faf641f831bccc
akihisa <sahikia117@gmail.com>
2024-09-20 20:06:06 +0900

    コード修正

--- atcoder/abc/abc077c.cpp
+++ atcoder/abc/abc077c.cpp
@@ -1,29 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-using vi = vector<int>;
+using ll = long long;
 #define all(a) (a).begin(), (a).end()
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
 int main() {
-  int N;
-  cin >> N;
-  vi A(N), B(N), C(N);
-  rep(i, N) cin >> A.at(i);
-  rep(i, N) cin >> B.at(i);
-  rep(i, N) cin >> C.at(i);
+  int N; cin >> N;
+  vector<int> A(N), B(N), C(N);
+  rep(i, N) cin >> A[i];
+  rep(i, N) cin >> B[i];
+  rep(i, N) cin >> C[i];
   
   sort(all(A));
-  sort(all(B));
   sort(all(C));
   
-  int64_t ans = 0;
+  ll ans = 0;
   rep(i, N) {
-    auto itA = lower_bound(all(A), B.at(i));
-    auto itC = upper_bound(all(C), B.at(i));
-    int64_t countA = distance(A.begin(), itA);
-    int64_t countC = distance(itC, C.end());
-    ans += countA * countC;
+    auto itA = lower_bound(all(A), B[i]);
+    auto itC = upper_bound(all(C), B[i]);
+    int a = distance(A.begin(), itA);
+    int c = N - distance(C.begin(), itC);
+    ans += (ll)a * c;
   }
   
   cout << ans << endl;
 }
+
