79eec8eeba5e0dc6e4b64c9310a369ad310a2056
hamutahana <164027820+hamutahana@users.noreply.github.com>
2024-09-21 13:26:16 +0900

    ファイル整理 (#12)

---
+++ atcoder/abc/abc317c.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+  int N, M; cin >> N >> M;
+  vector<vector<P>> G(N);
+  rep(i, M) {
+    int a, b, c; cin >> a >> b >> c;
+    a--; b--;
+    G[a].emplace_back(c, b);
+    G[b].emplace_back(c, a);
+  }
+  
+  int ans = 0;
+  vector<bool> visited(N, false);
+  auto f = [&](auto f, int v, int d) -> void {
+    visited[v] = true;
+    ans = max(ans, d);
+    for (auto [cost, nv] : G[v]) {
+      if (visited[nv]) continue;
+      f(f, nv, d + cost);
+    }
+    visited[v] = false;
+  };
+  rep(i, N) f(f, i, 0);
+  
+  cout << ans << endl;
+}
+
---
+++ atcoder/abc/abc327c.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using vi = vector<int>;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+bool check(vi &a) {
+  set<int> st;
+  for (int x : a) st.insert(x);
+  if (st.size() != 9) return false;
+  return true;
+}
+
+bool solve() {
+  vector<vi> a(9, vi(9));
+  rep(i, 9) rep(j, 9) cin >> a[i][j];
+  
+  // 行判定
+  rep(i, 9) if (!check(a[i])) return false;
+  // 列判定
+  rep(j, 9) {
+    vi b;
+    rep(i, 9) b.push_back(a[i][j]);
+    if (!check(b)) return false;
+  }
+  // ブロック判定
+  rep(p, 9) {
+    int i = (p / 3) * 3, j = (p % 3) * 3;
+    vi b;
+    rep(q, 9) {
+      int di = q / 3, dj = q % 3;
+      b.push_back(a[i + di][j + dj]);
+    }
+    if (!check(b)) return false;
+  }
+  return true;
+}
+
+int main() {
+  if (solve()) cout << "Yes" << endl;
+  else cout << "No" << endl;
+}
+
