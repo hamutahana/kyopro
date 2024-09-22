d3d898db521ab03778cc077c5741cb481e291123
hamutahana <164027820+hamutahana@users.noreply.github.com>
2024-09-22 23:34:15 +0900

    不要ファイルの削除 (#17)

--- atcoder/abc/abc342c.cpp
+++ atcoder/abc/abc342c.cpp
@@ -1,42 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-using vi = vector<int>;
-using vc = vector<char>;
-using vb = vector<bool>;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
-const int M = 26;
-
 int main() {
-  int N;
-  string S;
-  cin >> N >> S;
-  vc L(M);
-  rep(i, M) {
-    char c = 'a' + i;
-    L.at(i) = c;
-  }
-  int Q;
-  cin >> Q;
-  rep(i, Q) {
-    char x, y;
-    cin >> x >> y;
-    rep(i, M) {
-      if (L.at(i) == x) L.at(i) = y;
-    }
+  int N, Q; string S;
+  cin >> N >> S >> Q;
+  
+  map<char, char> mp;
+  rep(i, 26) mp['a' + i] = 'a' + i;
+  rep(qi, Q) {
+    char c, d; cin >> c >> d;
+    // bf: before, af: after
+    for (auto &[bf, af] : mp) if (af == c) af = d;
   }
   
-  vb is_changed(N, false);
-  rep(i, M) {
-    int bf_char = 'a' + i, af_char = L.at(i);
-    rep(i, N) {
-      if (S.at(i) == bf_char && !is_changed.at(i)) {
-        S.at(i) = af_char;
-        is_changed.at(i) = true;
-      }
-    }
-  }
-  
-  cout << S << endl;
+  string ans = "";
+  rep(i, S.size()) ans += mp[S[i]];
+  cout << ans << endl;
 }
 
