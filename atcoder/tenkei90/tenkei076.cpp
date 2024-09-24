2bf3777eedb16a7d25b95b1718d03bcbc1be4134
hamutahana <164027820+hamutahana@users.noreply.github.com>
2024-09-24 12:01:29 +0900

    ファイル修正 (#22)

--- atcoder/tenkei90/tenkei076.cpp
+++ atcoder/tenkei90/tenkei076.cpp
@@ -1,47 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
+using vi = vector<int>;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+bool solve() {
+  int N; cin >> N;
+  vi A(N); rep(i, N) cin >> A[i];
+  
+  const int M = 2*N;
+  vector<ll> S(M);
+  S[0] = 0;
+  for (int i = 1; i < M; i++) S[i] = S[i-1] + A[(i-1) % N];
+  
+  ll total = 0;
+  rep(i, N) total += A[i];
+  if (total % 10 != 0) return false;
+  
+  ll goal = total / 10;
+  rep(i, N) {
+    if (binary_search(S.begin(), S.end(), S[i] + goal)) return true;
+  }
+  return false;
+}
 
 int main() {
-  int N;
-  cin >> N;
-  vector<int64_t> A(N), B(2*N - 1);
-  rep(i, N) cin >> A.at(i);
-  rep(i, 2*N - 1) B.at(i) = A.at(i % N);
-  
-  int64_t total = 0;
-  for (int64_t x : A) total += x;
-  
-  if (total % 10 != 0) {
-    cout << "No" << endl;
-    return 0;
-  }
-  
-  int64_t target = total / 10;
-  
-  queue<int64_t> q;
-  int64_t sum = 0;
-  rep(i, 2*N - 1) {
-    if (B.at(i) > target) {
-      queue<int64_t> empty;
-      swap(q, empty);
-      sum = 0;
-      continue;
-    }
-    
-    q.push(B.at(i));
-    sum += B.at(i);
-    if (sum == target) {
-      cout << "Yes" << endl;
-      return 0;
-    }
-    
-    while (sum > target) {
-      int first_element = q.front();
-      sum -= first_element;
-      q.pop();
-    }
-  }
-  
-  cout << "No" << endl;
+  if (solve()) cout << "Yes" << endl;
+  else cout << "No" << endl;
 }
+
